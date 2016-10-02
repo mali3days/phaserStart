bf51f9c7aca261c5144b63b991131540fee76adb
Bugay Nikolay <mali3days@gmail.com>
2016-10-02 13:33:46 +0300

    Added music, songs: jump, coin, dead.

--- js/load.js
+++ js/load.js
@@ -16,6 +16,11 @@ var loadState = {
     game.load.image('wallV', 'assets/wallVertical.png');
     game.load.image('wallH', 'assets/wallHorizontal.png');
     game.load.image('background', 'assets/background.png');
+
+    game.load.audio('music', 'assets/background.mp3');
+    game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
+    game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
+    game.load.audio('dead', ['assets/dead.ogg', 'assets/dead.mp3']);
   },
 
   create: function() {
