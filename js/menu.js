80683d6942bca3f566af2a85649475f8ff484895
Nikolay Bugay <mali3days@gmail.com>
2016-11-04 01:06:14 +0200

    Best Score was added to local storage and display in the game menu.

--- js/menu.js
+++ js/menu.js
@@ -12,10 +12,9 @@ var menuState = {
                         .to({y: 80}, 1000)
                         .easing(Phaser.Easing.Bounce.Out)
                         .start();
-
-    var scoreLabel = game.add.text(game.world.centerX, game.world.centerY,
-      'score: ' + game.global.score,
-      {  font: '25px Arial', fill: '#ffffff' }
+    var text = 'score: ' + game.global.score + '\nbest score: ' + localStorage.getItem('bestScore');
+    var scoreLabel = game.add.text(game.world.centerX, game.world.centerY, text,
+      {  font: '25px Arial', fill: '#ffffff', align: 'center' }
     );
     scoreLabel.anchor.setTo(0.5, 0.5);
 
@@ -33,6 +32,14 @@ var menuState = {
 
     var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     upKey.onDown.addOnce(this.start, this);
+
+    if (!localStorage.getItem('bestScore')) {
+      localStorage.setItem('bestScore', 0);
+    }
+
+    if (game.global.score > localStorage.getItem('bestScore')) {
+      localStorage.setItem('bestScore', game.global.score);
+    }
   },
 
   start: function() {
